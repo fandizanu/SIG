bafa83550a7f96abb2772351c7f59c3558a4482f
fandizanu <zanukamil@gmail.com>
2022-03-22 14:46:30 +0800

    Ubah jadi Admin SIG Lagi

--- admin/headeradm.php
+++ admin/headeradm.php
@@ -20,7 +20,7 @@
     </div>
     <!-- Sidebar -->
     <div class="w3-sidebar w3-2020-navy-blazer w3-bar-block" style="width:13%">
-    <a class="w3-bar-item w3-button w3-hover-white" href="admin.php" style="font-size: 25px; border-bottom:3px solid grey; "><b>Menu Admin</b></a>
+    <a class="w3-bar-item w3-button w3-hover-white" href="admin.php" style="font-size: 25px; border-bottom:3px solid grey; "><b>Admin SIG</b></a>
     <a class="w3-bar-item w3-button w3-hover-white" href="transformasiadmin.php"><img src="../asset/setting.png" style="height:12px;width:12px;"> Tranformasi Data</a>
     <a class="w3-bar-item w3-button w3-hover-white" href="clusteradmin.php"> <img src="../asset/clstr.png" style="height:12px;width:12px;"> Cluster</a>
     <a class="w3-bar-item w3-button w3-hover-white" href="atributadmin.php"> <img src="../asset/attr.png" style="height:12px;width:12px;"> Atribut</a>
